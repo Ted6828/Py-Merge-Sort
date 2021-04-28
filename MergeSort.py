7c01bbb2f89a9f88dc1a93c64904527bcb43c9bf
Ted6828 <77334264+Ted6828@users.noreply.github.com>
2021-04-28 17:01:27 +0100

    Update MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -8,6 +8,7 @@ def Merge(Array):
         Array2 = list.copy(Array)
         for i in range (HalfLength):
              Array1.pop(len(Array1)-1)
+        for i in range (length - HalfLength):
              Array2.pop(0)
         Array1 = Merge(Array1)
         Array2 = Merge(Array2)
@@ -35,8 +36,8 @@ def Merge2(Array1, Array2):
             Var1 = Var1 + 1
     return Array3
 
-Values = int(input("Enter the number of values (Has to be even!) ... : "))
-for i in range(0, Values):
+Values = int(input("Enter the number of values... : "))
+for i in range(Values):
     pog = int(input())
     MergeSort.append(pog)
 print(*Merge(MergeSort),end=" ...Is the sorted list!")
